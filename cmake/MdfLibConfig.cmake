4e1cc7d770d73e725f3daffc38e08bad89aa477d
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-10-08 18:27:05 +0200

    add expat as requirement

--- cmake/MdfLibConfig.cmake
+++ cmake/MdfLibConfig.cmake
@@ -2,4 +2,4 @@ include("${CMAKE_CURRENT_LIST_DIR}/MdfLibTargets.cmake")
 
 ## adding this does not change anything
 #find_package(ZLIB REQUIRED)
-#find_package(EXPAT REQUIRED)
+find_package(EXPAT REQUIRED)

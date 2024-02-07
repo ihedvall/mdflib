16b8f111ee66388ed741687445674cdefc92a932
Tiago Porsch Dopke <tiagoporsch@icloud.com>
2024-02-07 15:51:54 -0300

    Fixed missing <cstdlib> includes Caused errors when compiling with GCC

--- include/mdf/etag.h
+++ include/mdf/etag.h
@@ -13,6 +13,7 @@
  */
 #pragma once
 
+#include <cstdint>
 #include <sstream>
 #include <string>
 #include <vector>
--- include/mdf/zlibutil.h
+++ include/mdf/zlibutil.h
@@ -7,6 +7,7 @@
  */
 
 #pragma once
+#include <cstdint>
 #include <cstdio>
 #include <string>
 #include <vector>

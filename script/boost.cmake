b8f7cf9a789c729b1c22d084e2f46b4c31ab9d2b
ihedvall <92839244+ihedvall@users.noreply.github.com>
2026-01-15 17:37:14 +0100

    #153 Fixed CMake installation path
    Upgraded to boost 1.90

--- script/boost.cmake
+++ script/boost.cmake
@@ -1,6 +1,8 @@
 # Copyright 2021 Ingemar Hedvall
 # SPDX-License-Identifier: MIT
 
+include(CMakePrintHelpers)
+
 if (NOT Boost_FOUND)
     set(Boost_USE_STATIC_LIBS ON)
     set(Boost_USE_MULTITHREADED ON)
@@ -8,17 +10,21 @@ if (NOT Boost_FOUND)
     set(Boost_NO_WARN_NEW_VERSIONS ON)
     set(Boost_DEBUG OFF)
 
-    if (COMP_DIR)
-        #set(Boost_DIR ${COMP_DIR}/boost/latest/lib/cmake/Boost-1.86.0)
-        set(Boost_ROOT ${COMP_DIR}/boost/latest)
+    find_package(Boost CONFIG COMPONENTS filesystem locale program_options iostreams)
+    if (NOT Boost_FOUND)
+        if (COMP_DIR)
+            set(Boost_ROOT ${COMP_DIR}/boost/latest/lib/cmake)
+        endif()
+        find_package(Boost REQUIRED CONFIG COMPONENTS filesystem locale program_options iostreams)
     endif()
 
-    find_package(Boost REQUIRED CONFIG COMPONENTS filesystem system locale program_options iostreams process)
-    message(STATUS "Boost Found : " ${Boost_FOUND})
-    message(STATUS "Boost Version: " ${Boost_VERSION_STRING})
-    message(STATUS "Boost Include Dirs: " ${Boost_INCLUDE_DIRS})
-    message(STATUS "Boost Library Dirs: " ${Boost_LIBRARY_DIRS})
-    message(STATUS "Boost Libraries: " ${Boost_LIBRARIES})
 
 endif()
+
+cmake_print_variables(
+        Boost_FOUND
+        Boost_VERSION_STRING
+        Boost_INCLUDE_DIRS
+        Boost_LIBRARY_DIRS
+        Boost_LIBRARIES)
 
